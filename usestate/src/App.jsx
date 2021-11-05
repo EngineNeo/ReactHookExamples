16723933f780fe8147677fe06e71bd567abc8f51
Neo Maralit <neomaralit@gmail.com>
2021-11-04 22:12:32 -0400

    completed usestate hook example

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [count, setCount] = useState(4)
+  const [count, setCount] = useState(4) //Use State can be seperated
+  const [theme, setTheme] = useState('blue') //Into multiple hooks
+  // const count = state.count; // Creates a variable count using the state
+  // const theme = state.theme; // A theme variable using state in the array
 
   function deCount() {
-    setCount(prevCount => prevCount - 1)
+      setCount(prevCount => prevCount - 1)
+      setTheme('Green')
   }
 
   function inCount() {
     setCount(prevCount => prevCount + 1)
+    setTheme('Red')
   }
 
   return (
     <>
       <button onClick={deCount}>-</button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={inCount}>+</button>
     </>
   )
